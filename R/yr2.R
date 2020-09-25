c3058de38fc6184430ed78bb6e23797a518dbb05
Jochen Weile <jochenweile@gmail.com>
2020-09-25 15:15:50 -0400

    documentation and experimental helper function for confidence intervals

--- R/yr2.R
+++ R/yr2.R
@@ -1,11 +1,44 @@
 
 
-# truth <- c(rep(TRUE,10),rep(FALSE,8))
-# scores <- cbind(
-#   c(rnorm(10,1,0.2),rnorm(8,0.8,0.1)),
-#   c(rnorm(10,1,0.2),rnorm(8,0.8,0.2))
-# )
+#' confidence interval for precision or recall
+#'
+#' @param i the number of correct calls
+#' @param n the number of total calls
+#' @param p the confidence interval probability range e.g. 0.025;0.975
+#' @param res the resolution at which to sample the call rates
+#'
+#' @return the confidence interval (numerical vector)
+prcCI <- function(i,n,p=c(0.025,0.975),res=0.001) {
+  rates <- seq(0,1,res)
+  dens <- dbinom(i,n,rates)
+  cdf <- c(0,cumsum(dens[-1]*res)/sum(dens*res))
+  # plot(rates,cdf,type="l")
+  sapply(p,function(.p) rates[max(which(cdf < .p))])
+}
 
+#' YogiRoc2 object constructor
+#'
+#' @param truth a boolean vector indicating the classes of the reference set
+#' @param scores a matrix of scores, with rows for each entry in truth, and one column for each predictor
+#' @param names the names of the predictors
+#' @param high a boolean vector indicating for each predictor whether its scoring high-to-low (or low-to-high)
+#'
+#' @return a yogiroc2 object
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #draw PRC curve
+#' draw.prc(yrobj)
+#' #calculate recall at 90% precision
+#' recall.at.prec(yrobj,0.9)
 yr2 <- function(truth, scores, names=colnames(scores), high=TRUE) {
   
   #make sure all input is of correct datatype
@@ -48,8 +81,12 @@ yr2 <- function(truth, scores, names=colnames(scores), high=TRUE) {
       tpr.sens <- tp/(tp+fn)
       fpr.fall <- fp/(tn+fp)
       ppv.prec.balanced <- ppv.prec*(1-prior)/(ppv.prec*(1-prior)+(1-ppv.prec)*prior)
+      #assess the confidence intervals
+      precCI <- prcCI(tp,tp+fp)
       #return the results
-      cbind(thresh=t,ppv.prec=ppv.prec,tpr.sens=tpr.sens,fpr.fall=fpr.fall,ppv.prec.balanced=ppv.prec.balanced)
+      cbind(thresh=t,ppv.prec=ppv.prec,tpr.sens=tpr.sens,fpr.fall=fpr.fall,
+            ppv.prec.balanced=ppv.prec.balanced,precCI5=precCI[[1]],precCI95=precCI[[2]]
+      )
     }))
     #set precision at infinite score threshold to 1
     data[nrow(data),c("ppv.prec","ppv.prec.balanced")] <- 1
@@ -73,6 +110,15 @@ yr2 <- function(truth, scores, names=colnames(scores), high=TRUE) {
   return(structure(tables,class="yr2"))
 }
 
+
+#' print method for yogiroc2 objects
+#'
+#' @param yr2 the object
+#'
+#' @return nothing, just prints a description
+#' @export
+#'
+#' @examples
 print.yr2 <- function(yr2) {
   cat("YogiROC object\n")
   cat("Reference set size:",nrow(yr2[[1]]-2),"\n")
@@ -80,7 +126,27 @@ print.yr2 <- function(yr2) {
 }
 
 
-#plot FPR vs sensitivity
+#' Draw a ROC curve
+#'
+#' @param yr2 an underlying yogiroc2 object
+#' @param col the colors to use for the predictors
+#' @param legend the positioning of the legend (e.g. "bottomright). NA to disable legend.
+#' @param ... additional graphical parameters (see \code{par})
+#'
+#' @return nothing, draws a plot
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #draw PRC curve
+#' draw.roc(yrobj)
 draw.roc <- function(yr2,col=seq_along(yr2),legend="bottomright",...) {
   stopifnot(inherits(yr2,"yr2"))
   plot(
@@ -103,7 +169,33 @@ draw.roc <- function(yr2,col=seq_along(yr2),legend="bottomright",...) {
 }
 
 
-#Plot Recall vs Precision
+#' Draw Precision-Recall Curve (PRC)
+#'
+#' @param yr2 the yogiroc2 object
+#' @param col vector of colors to use for the predictors
+#' @param monotonized whether or not to monotonized the curve
+#' @param balanced whether or not to use prior-balancing
+#' @param legend the position of the legend, e.g. "bottomleft". NA disables legend
+#' @param ... additional graphical parameters (see \code{par})
+#'
+#' @return nothing. draws a plot
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #draw PRC curve
+#' draw.prc(yrobj)
+#' #draw non-monotonized PRC curve
+#' draw.prc(yrobj,monotonized=FALSE)
+#' #draw balanced PRC curve
+#' draw.prc(yrobj,balanced=TRUE)
 draw.prc <- function(yr2,col=seq_along(yr2),monotonized=TRUE,balanced=FALSE,legend="bottomleft",...) {
   stopifnot(inherits(yr2,"yr2"))
   ppvcol <- if (balanced) {
@@ -131,6 +223,30 @@ draw.prc <- function(yr2,col=seq_along(yr2),monotonized=TRUE,balanced=FALSE,lege
 }
 
 
+#' Calculate area under precision recall curve (AUPRC)
+#'
+#' @param yr2 the yogiroc2 object
+#' @param monotonized whether or not use a monotonized PRC curve
+#' @param balanced whether or not to use prior-balancing
+#'
+#' @return a numerical vector with the AUPRC values for each predictor
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #calculate AUPRC
+#' auprc(yrobj)
+#' #calculate non-monotonized AUPRC
+#' auprc(yrobj,monotonized=FALSE)
+#' #calculate balanced AUPRC
+#' auprc(yrobj,balanced=TRUE)
 auprc <- function(yr2, monotonized=TRUE, balanced=FALSE) {
   stopifnot(inherits(yr2,"yr2"))
   ppvcol <- if (balanced) {
@@ -147,7 +263,25 @@ auprc <- function(yr2, monotonized=TRUE, balanced=FALSE) {
   })
 }
   
-#calculate AUC for ROC curve
+
+#' Calculate the area under the ROC curve
+#'
+#' @param yr2 the yogiroc2 object
+#'
+#' @return a numerical vector with the AUROC for each predictor
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #calculate AUROC
+#' auroc(yrobj)
 auroc <- function(yr2) {
   stopifnot(inherits(yr2,"yr2"))
   sapply(yr2,function(data) {
@@ -163,7 +297,31 @@ auroc <- function(yr2) {
   })
 }
 
-#return maximum recall at given minimum precision
+#' Calculate maximum recall at given minimum precision
+#'
+#' @param yr2 the yogiroc2 object
+#' @param x the precision cutoff (default 0.9)
+#' @param monotonized whether or not to use monotonized PRC
+#' @param balanced whether or not to use prior-balancing
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' #generate fake data
+#' truth <- c(rep(TRUE,10),rep(FALSE,8))
+#' scores <- cbind(
+#'   pred1=c(rnorm(10,1,0.2),rnorm(8,.9,0.1)),
+#'   pred2=c(rnorm(10,1.1,0.2),rnorm(8,.9,0.2))
+#' )
+#' #create yogiroc2 object
+#' yrobj <- yr2(truth,scores)
+#' #calculate R90P
+#' recall.at.prec(yrobj)
+#' #calculate non-monotonized R90P
+#' recall.at.prec(yrobj,monotonized=FALSE)
+#' #calculate balanced R90P
+#' recall.at.prec(yrobj,balanced=TRUE)
 recall.at.prec <- function(yr2,x=0.9,monotonized=TRUE,balanced=FALSE) {
   stopifnot(inherits(yr2,"yr2"))
   ppvcol <- if (balanced) {
